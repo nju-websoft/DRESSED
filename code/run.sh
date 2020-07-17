bc7cf6a6ff2934027ece4b4354d47e7e7fcb53b8
unknown <yjydfnhc@126.com>
2020-07-17 20:55:38 +0800

    run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 declare -a ds_list=("dbpedia" "lmdb" "dsfaces")
 
-for ds_name in ${ds_list[@]}; do
+for ds_name in "${ds_list[@]}"
+do
 echo $ds_name
 for ((repidx=0; repidx<25; repidx++))
 do
 runname="run$repidx"
 echo "$runname"
-command="python -m ies_train.train_multirun $ds_name $runname -epoch_num 50 -dev_step 1 -batch_size 16"
+command="python -m ies_train.train_multirun $ds_name $runname -epoch_num 50 -dev_step 1 -batch_size 1"
 echo "$command"
 if eval $command; then
 	echo "command succeeded"
